c4dc1655b8694af76961f60fe45977443f6d5605
pablomr96 <pablomr960@gmail.com>
2016-04-28 19:21:17 +0200

    subido

---
+++ Apartado_2/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <time.h>
+#include "quaternion.h"
+#include "rutinas_clock.h"
+
+int main() {	
+	quaternion *lista_A;
+	quaternion *lista_B;
+	quaternion *lista_C;	
+    quaternion *lista_DP;
+	int i;	
+	float x, y, z, w;
+    double ck=0;
+
+    FILE *fp;
+    if((fp = fopen("puntos.txt","a")) == NULL)
+        perror("en la apertura del archivo");    
+
+	lista_A = genera_lista_quaternion();
+	lista_B = genera_lista_quaternion();
+	lista_C = genera_lista_quaternion();
+    lista_DP = genera_lista_quaternion();
+
+	srand(time(NULL));
+
+	quaternion *aux;
+	for(i=0; i<N; i++) {
+		x = genera_float(); y = genera_float(); z = genera_float(); w = genera_float();
+		aux = genera_quaternion(x, y, z, w);	
+		anhade_elemento_lista(lista_A, *aux, i);		
+	}
+	for(i=0; i<N; i++) {
+		x = genera_float(); y = genera_float(); z = genera_float(); w = genera_float();
+		aux = genera_quaternion(x, y, z, w);	
+		anhade_elemento_lista(lista_B, *aux, i);		
+	}
+	for(i=0; i<N; i++) {
+		x = genera_float(); y = genera_float(); z = genera_float(); w = genera_float();
+		aux = genera_quaternion(x, y, z, w);	
+		anhade_elemento_lista(lista_C, *aux, i);		
+	}
+    for(i=0; i<N ;i++){
+        x = 0; y = 0; z = 0; w = 0;
+        aux = genera_quaternion(x, y, z, w);
+        anhade_elemento_lista(lista_DP, *aux, i);
+    }
+	
+	
+	for(i=0; i<N ;i++){
+        x = 0; y = 0; z = 0; w = 0;
+        aux = genera_quaternion(x, y, z, w);
+        anhade_elemento_lista(lista_DP, *aux, i);
+    }
+   
+    
+    start_counter();
+    for(i=0; i<20; i++){
+        multiplica_lista_quaternion(lista_A,lista_B,lista_C);        
+        segunda_computacion(lista_C, lista_DP, lista_DP);
+    }
+    ck=get_counter();
+    ck=ck/(N*20);
+    
+    printf("Ciclos de acceso a un quaternion: %f\n",ck);
+    fprintf(fp,"%f  ",ck);
+    fprintf(fp,"%d\n",N);
+    fclose(fp);
+    
+    printf("LISTA 1:\n");
+	imprime_lista_quaternion(lista_A);
+	printf("LISTA 2:\n");
+	imprime_lista_quaternion(lista_B);
+	printf("LISTA 3:\n");
+	imprime_lista_quaternion(lista_C);
+    printf("LISTA FINAL:\n");
+    imprime_lista_quaternion(lista_DP);
+
+	libera_lista_quaternion(lista_A);
+	libera_lista_quaternion(lista_B);
+	libera_lista_quaternion(lista_C);
+}	
---
+++ Apartado_2/quaternion.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <time.h>
+#include <unistd.h>
+#include <pmmintrin.h>
+
+#define N 10000
+
+typedef struct {
+	float comp[4];
+} quaternion;
+
+float genera_float() {
+	float random = ((float) rand()) / (float) RAND_MAX;
+
+	return random;
+}
+
+quaternion *genera_quaternion(float x, float y, float z, float w) {	
+	quaternion *q = _mm_malloc(sizeof(quaternion),512);
+	
+	if(q != NULL) {
+		q->comp[0] = x; q->comp[1] = y; q->comp[2] = z; q->comp[3] = w;
+	}	
+	return q;
+}
+
+
+void imprime_quaternion(quaternion aux) {
+	printf("{ %f, %f, %f, %f }\n", aux.comp[0], aux.comp[1], aux.comp[2], aux.comp[3]);
+}
+
+void libera_quaternion(quaternion *aux) {
+	if(aux != NULL)
+		_mm_free(aux);
+}
+
+quaternion *genera_lista_quaternion() {
+	quaternion *v = _mm_malloc(N * sizeof(quaternion),512);
+
+	return v;
+}
+
+void anhade_elemento_lista(quaternion *lista, quaternion elemento, int pos) {
+	if(lista != NULL)
+		*(lista + pos) = elemento;
+}
+
+void imprime_lista_quaternion(quaternion *lista) {
+	quaternion aux;
+	int i;
+	for(i=0; i<N; i++) {
+		aux = *(lista + i);
+		imprime_quaternion(aux);
+	}		
+}
+
+void libera_lista_quaternion(quaternion *lista) {
+    _mm_free(lista);
+}
+
+
+void suma_quaternion(quaternion A, quaternion B, quaternion *R){
+    int i=0;
+    for(i=0; i<4; i++){
+        R->comp[i] = A.comp[i] + B.comp[i];
+    }
+}
+
+
+void suma_lista_quaternion(quaternion *listaA, quaternion *listaB, quaternion *listaR){
+    int i=0;
+    for(i=0; i<N/2; i++){
+        suma_quaternion(*(listaA + i), *(listaB + i), (listaR + i));
+        suma_quaternion(*(listaA + (i+1)), *(listaB + (i+1)), (listaR + (i+1)));
+    }
+}
+
+void multiplica_quaternion(quaternion A, quaternion B, quaternion *R){
+    R->comp[0] = A.comp[0] * B.comp[0] - A.comp[1] * B.comp[1] - A.comp[2] * B.comp[2] - A.comp[3] * B.comp[3];
+    R->comp[1] = A.comp[0] * B.comp[1] + A.comp[1] * B.comp[0] + A.comp[2] * B.comp[3] - A.comp[3] * B.comp[2];
+    R->comp[2] = A.comp[0] * B.comp[2] - A.comp[1] * B.comp[3] + A.comp[2] * B.comp[0] + A.comp[3] * B.comp[1];
+    R->comp[3] = A.comp[0]*B.comp[3] + A.comp[1]*B.comp[2] - A.comp[2]*B.comp[1] + A.comp[3]*B.comp[0];  
+}
+
+void multiplica_lista_quaternion(quaternion *listaA, quaternion *listaB, quaternion *listaR){
+    int i=0;
+    for(i=0; i<N; i++){
+        multiplica_quaternion(*(listaA + i), *(listaB + i), (listaR + i));
+    }
+}
+
+void segunda_computacion(quaternion *listaA, quaternion *listaAux, quaternion *listaR){
+    multiplica_lista_quaternion(listaA, listaA, listaAux);
+    suma_lista_quaternion(listaAux, listaR, listaR);
+}
+	
